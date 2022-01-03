e971cf8d0b4528ad7ce7f391925c05450d2a2502
Felipe Santos <felipecassiors@gmail.com>
2022-01-03 16:48:10 -0300

    Run asterisk as asterisk user
    Plus some code cleanup. I also formatted the file using shfmt.

--- Asterisk/run.sh
+++ Asterisk/run.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/with-contenv bashio
+# shellcheck shell=bash
 
 bashio::log.info "Checking config files..."
 
 if ! bashio::fs.directory_exists '/config/asterisk'; then
-    mkdir -p /config/asterisk \
-        || bashio::exit.nok 'Failed to create initial asterisk config folder'
+    mkdir -p /config/asterisk ||
+        bashio::exit.nok 'Failed to create initial asterisk config folder'
 fi
 
 AMI_PASSWORD=$(bashio::config 'ami_password')
 HA_IP=$(getent hosts homeassistant | awk '{ print $1 }')
 
-cat <<'EOF' > '/etc/asterisk/manager.conf'
+cat <<'EOF' >'/etc/asterisk/manager.conf'
 [general]
 enabled = yes
 port = 5038
@@ -38,13 +39,13 @@ tlsenable=yes
 tlsbindaddr=0.0.0.0:8089
 tlscertfile=/etc/asterisk/keys/asterisk.pem
 tlsprivatekey=/etc/asterisk/keys/asterisk.pem
-' > '/etc/asterisk/http.conf'
+' >'/etc/asterisk/http.conf'
 
 echo '
 [general]
 rtpstart=10000
 rtpend=10008
-' > '/etc/asterisk/rtp.conf'
+' >'/etc/asterisk/rtp.conf'
 
 echo $'
 [general]
@@ -73,13 +74,13 @@ dtmfmode=rfc2833
 [my-codecs](!)
 allow=!all,ulaw,alaw,speex,gsm,g726,g723\n
 \n
-' > '/etc/asterisk/sip.conf'
+' >'/etc/asterisk/sip.conf'
 
-sed -i 's/noload => chan_sip.so/;noload => chan_sip.so/' /etc/asterisk/modules.conf > /dev/null
+sed -i 's/noload => chan_sip.so/;noload => chan_sip.so/' /etc/asterisk/modules.conf >/dev/null
 
 if ! bashio::fs.file_exists '/config/asterisk/sip.conf'; then
-    cp -a /etc/asterisk/. /config/asterisk/ \
-        || bashio::exit.nok 'Failed to make sample configs'
+    cp -a /etc/asterisk/. /config/asterisk/ ||
+        bashio::exit.nok 'Failed to make sample configs'
 fi
 
 bashio::log.info "Creating certificate..."
@@ -87,12 +88,12 @@ bashio::log.info "Creating certificate..."
 # REPLACE WITH CERTBOT
 openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 \
     -subj "/C=NL/ST=Denial/L=Amsterdam/O=Dis/CN=Asterisk" \
-    -keyout /etc/asterisk/keys/asterisk.key  -out /etc/asterisk/keys/asterisk.cert > /dev/null
+    -keyout /etc/asterisk/keys/asterisk.key -out /etc/asterisk/keys/asterisk.cert >/dev/null
 
-cat /etc/asterisk/keys/asterisk.key > /etc/asterisk/keys/asterisk.pem
-cat /etc/asterisk/keys/asterisk.cert >> /etc/asterisk/keys/asterisk.pem
+cat /etc/asterisk/keys/asterisk.key >/etc/asterisk/keys/asterisk.pem
+cat /etc/asterisk/keys/asterisk.cert >>/etc/asterisk/keys/asterisk.pem
 
-cp -a -f  /etc/asterisk/keys/. /config/asterisk/keys/ || bashio::exit.nok 'Failed to update certificate'
+cp -a -f /etc/asterisk/keys/. /config/asterisk/keys/ || bashio::exit.nok 'Failed to update certificate'
 
 bashio::log.info "Configuring Asterisk..."
 
@@ -103,66 +104,16 @@ AUTO_ADD=$(bashio::config 'auto_add')
 
 if $AUTO_ADD; then
     EXTENSION=100
-    for person in ${PERSONS}
-    do
-    EXTENSION=$((${EXTENSION}+1))
-    echo "
+    for person in ${PERSONS}; do
+        EXTENSION=$((${EXTENSION} + 1))
+        echo "
 [${EXTENSION}](sipjs-phone,my-codecs)
 username=${EXTENSION}
 secret=1234
-    " >> '/etc/asterisk/sip.conf'
+    " >>'/etc/asterisk/sip.conf'
     done
 fi
 
 bashio::log.info "Starting Asterisk..."
 
-exec asterisk -vvvvvvvvv -ddddddddddd -f
-
-# RUN ANOTHER SCRIPT UPDATING THE ENTITIES AND KEEP ASTERISK CONSOLE HERE
-
-# sleep 10
-
-# while true
-# do
-#     echo " "
-#     bashio::log.info $(asterisk -x "sip show peers")
-#     sleep 5
-#     echo " "
-#     bashio::log.info $(asterisk -x "http show status")
-#     sleep 5
-#     echo " "
-#     if $AUTO_ADD; then
-#         EXTENSION=100
-#         for person in ${PERSONS}
-#         do
-#             EXTENSION=$((${EXTENSION}+1))
-#             if asterisk -x "sip show peers" | grep "${EXTENSION}" | grep '(Unspecified)' > /dev/null; then
-#                 STATE='off'
-#             else
-#                 STATE='on'
-#             fi
-#             curl -s -X POST -H "Authorization: Bearer ${SUPERVISOR_TOKEN}" -H "Content-Type: application/json" -d '{"state": "'"${STATE}"'", "attributes": {"extension": "'"${EXTENSION}"'", "secret": "1234", "user": "'"${person}"'"}}' "http://supervisor/core/api/states/binary_sensor.sip_${person}" > /dev/null
-#         done
-#     fi
-
-#     for extension in $(bashio::config 'custom_extensions|keys')
-#     do
-#         EXTENSION=$(bashio::config "custom_extensions[${extension}].extension")
-#         NAME=$(bashio::config "custom_extensions[${extension}].name")
-#         TYPE=$(bashio::config "custom_extensions[${extension}].type")
-#         if [ $TYPE = "chan_sip" ]; then
-#             if asterisk -x "sip show peers" | grep "${EXTENSION}" | grep '(Unspecified)' > /dev/null; then
-#                 STATE='off'
-#             else
-#                 STATE='on'
-#             fi
-#         else
-#             if asterisk -x "pjsip show endpoints" | grep "${EXTENSION}" | grep '(Unspecified)' > /dev/null; then
-#                 STATE='off'
-#             else
-#                 STATE='on'
-#             fi
-#         fi
-#         curl -s -X POST -H "Authorization: Bearer ${SUPERVISOR_TOKEN}" -H "Content-Type: application/json" -d '{"state": "'"${STATE}"'", "attributes": {"extension": "'"${EXTENSION}"'", "user": "'"${NAME}"'"}}' "http://supervisor/core/api/states/binary_sensor.sip_${NAME}" > /dev/null
-#     done
-# done
+exec asterisk -U asterisk -vvvdddf
