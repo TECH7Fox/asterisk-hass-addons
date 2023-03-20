150120d336546d2b71626e28d3e9d29ed5b1d32d
Felipe Santos <felipecassiors@gmail.com>
2023-03-20 20:22:15 -0300

    Fix stdin service

--- asterisk/DOCS.md
+++ asterisk/DOCS.md
@@ -88,20 +88,14 @@ You can get a key [here](https://cloud.google.com/speech-to-text). Google says i
 The log level to configure Asterisk to use. To know more about the existing presets, check [`logger.conf`](./rootfs/usr/share/tempio/logger.conf.gtpl).
 
 ### STDIN service
-You can use the STDIN service to reload your configuration without restarting the entire add-on.
 
-The services now include:
+You can use the STDIN service to run any Asterisk CLI commands that you want. For example:
 
-- `reload_pjsip`
-- `reload_dialplan`
-
-Example:
-
-```
+```yaml
 service: hassio.addon_stdin
-    data:
-      addon: asterisk
-      input: reload_pjsip
+  data:
+    addon: asterisk
+    input: dialplan reload
 ```
 
 ## Configuring the [Asterisk integration](https://github.com/TECH7Fox/Asterisk-integration)
--- asterisk/rootfs/stdin.sh
+++ asterisk/rootfs/stdin.sh
@@ -1,39 +1,14 @@
 #!/usr/bin/with-contenv bashio
-# ==============================================================================
-# Home Assistant Community Add-on: Asterisk
-# Starts the STDIN service
-# ==============================================================================
-
 # shellcheck shell=bash
 
-bashio::log.info 'Starting the Home Assistant STDIN service...'
+bashio::log.info 'Starting the STDIN service for Home Assistant...'
 
 # shellcheck disable=SC2162
-while read cmd; do
-  cmd="${cmd%\"}"
-  cmd="${cmd#\"}"
+while read -r input; do
+    # Parse JSON value
+    input=$(bashio::jq "${input}" '.')
 
-  bashio::log.info "Received external command: ${cmd}"
-
-  case "${cmd}" in
-
-        "reload_pjsip")
-            bashio::log.info "Reloading PJSIP..."
-            cp /config/asterisk/pjsip_custom.conf /etc/asterisk/pjsip_custom.conf
-            cp /config/asterisk/pjsip.conf /etc/asterisk/pjsip.conf
-            asterisk -x module reload res_pjsip
-        ;;
-
-        "reload_dialplan")
-            bashio::log.info "Reloading dialplan..."
-            cp /config/asterisk/extensions.conf /etc/asterisk/extensions.conf
-            asterisk -x dialplan reload
-        ;;
-
-        *)
-            bashio::log.warning "Unknown STDIN command: ${cmd}"
-        ;;
-
-    esac
-
-done < /proc/1/fd/0
+    bashio::log.info "Executing command from stdin: asterisk -rx '${input}'"
+    # Ignore failures, otherwise the add-on would be restarted
+    asterisk -rx "${input}" || true
+done
