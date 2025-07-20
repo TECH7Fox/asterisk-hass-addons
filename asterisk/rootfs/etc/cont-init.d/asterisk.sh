c2ac0533831b8eed4cb48326af9c2fd5fd076bc8
Felipe Santos <felipecassiors@gmail.com>
2025-07-20 00:09:07 -0300

    Revert "Update config path"
    This reverts commit f71ed3fe2bf465287a7432f5b68c3273f18a7ab7.

--- asterisk/rootfs/etc/cont-init.d/asterisk.sh
+++ asterisk/rootfs/etc/cont-init.d/asterisk.sh
@@ -38,7 +38,7 @@ fi
 readonly ha_token="${HA_TOKEN:-"${SUPERVISOR_TOKEN:-}"}"
 
 readonly etc_asterisk="/etc/asterisk"
-readonly config_dir="/config"
+readonly config_dir="/config/asterisk"
 readonly default_config_dir="${config_dir}/default"
 readonly custom_config_dir="${config_dir}/custom"
 
